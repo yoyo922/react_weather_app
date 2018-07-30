351effb79d41cd7309a5c992128e51bcfae34f12
Peter <yzhou09@mail.uoguelph.ca>
2018-07-30 02:06:18 -0400

    creating report for weather app

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = 'ceaabd1441aec5eb12417e86fc7f7b4f';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather (city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type : FETCH_WEATHER,
+    payload: request
+  };
+};
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SearchBar from '../containers/searchBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+}
